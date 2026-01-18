07152fe59d1527946c7d4f3ebac283ed117c6f14
Aly Raffauf <aly@aly.codes>
2026-01-18 01:03:53 -0500

    app: doc cleanup

--- src/app.go
+++ src/app.go
@@ -19,21 +19,17 @@ const (
 	IssueURL   = "https://github.com/alyraffauf/switchyard/issues"
 	DonateURL  = "https://ko-fi.com/alyraffauf"
 )
-
-// Contributor represents a person who contributed to the project
 type Contributor struct {
 	Name string
 	URL  string
 }
 
-// Contributors is a list of people who contributed to the project
 var Contributors = []Contributor{
 	{Name: "Aly Raffauf", URL: "https://github.com/alyraffauf"},
 }
 
 // getAppID returns the application ID.
 // When running in Flatpak, it uses the FLATPAK_ID environment variable
-// to support both production and development (.Devel) builds.
 func getAppID() string {
 	if flatpakID := os.Getenv("FLATPAK_ID"); flatpakID != "" {
 		return flatpakID
