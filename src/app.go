7ac1a0ae172a031e4ae4b382ad9c76d6b70b1bb3
Aly Raffauf <aly@aly.codes>
2026-01-17 21:24:37 -0500

    tag for 0.9.0

--- src/app.go
+++ src/app.go
@@ -12,7 +12,7 @@ const (
 	AppName       = "Switchyard"
 	DeveloperName = "Aly Raffauf"
 	Copyright     = "© 2026 Aly Raffauf"
-	Version       = "0.8.2"
+	Version       = "0.9.0"
 
 	// Links
 	WebsiteURL = "https://github.com/alyraffauf/switchyard"
