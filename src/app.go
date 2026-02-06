bc11a527d4f8d3be5b55a1f57a54a0549fe3a769
Aly Raffauf <aly@aly.codes>
2026-02-06 14:43:44 -0500

    update for v0.12.0

--- src/app.go
+++ src/app.go
@@ -12,7 +12,7 @@ const (
 	AppName       = "Switchyard"
 	DeveloperName = "Aly Raffauf"
 	Copyright     = "© 2026 Aly Raffauf"
-	Version       = "0.11.4"
+	Version       = "0.12.0"
 
 	// Links
 	WebsiteURL = "https://switchyard.aly.codes/"
