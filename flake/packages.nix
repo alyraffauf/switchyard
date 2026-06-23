c4059bc350840c872f73f226cb9618ac0882700e
Aly Raffauf <aly@aly.codes>
2026-06-23 14:28:28 -0400

    nix: move package to package.nix

--- flake/packages.nix
+++ flake/packages.nix
@@ -17,48 +17,7 @@
         build-system = with pkgs.python3.pkgs; [setuptools];
       };
 
-      switchyard = pkgs.buildGoModule {
-        pname = "switchyard";
-        version = "dev";
-        src = inputs.self;
-        vendorHash = "sha256-1rEtf5QJA5xaeL0LRp13dwtPCH+DM5Wp4kJXBBqNtEg=";
-        subPackages = ["src"];
-
-        buildPhase = ''
-          runHook preBuild
-          go build -trimpath -ldflags="-s -w" -o $GOPATH/bin/switchyard ./src
-          runHook postBuild
-        '';
-
-        nativeBuildInputs = with pkgs; [
-          pkg-config
-          wrapGAppsHook4
-        ];
-
-        buildInputs = with pkgs; [
-          glib
-          gobject-introspection
-          gtk4
-          libadwaita
-          pkg-config
-        ];
-
-        postInstall = ''
-          install -Dm644 data/io.github.alyraffauf.Switchyard.desktop \
-            $out/share/applications/io.github.alyraffauf.Switchyard.desktop
-
-          install -Dm644 data/icons/hicolor/scalable/apps/io.github.alyraffauf.Switchyard.svg \
-            $out/share/icons/hicolor/scalable/apps/io.github.alyraffauf.Switchyard.svg
-        '';
-
-        meta = with pkgs.lib; {
-          description = "A configurable default browser for Linux";
-          homepage = "https://github.com/alyraffauf/switchyard";
-          license = licenses.gpl3Plus;
-          platforms = platforms.linux;
-          mainProgram = "switchyard";
-        };
-      };
+      switchyard = pkgs.callPackage ../package.nix {};
     };
   };
 }
---
+++ package.nix
@@ -0,0 +1,51 @@
+{
+  buildGoModule,
+  glib,
+  gobject-introspection,
+  gtk4,
+  libadwaita,
+  lib,
+  pkg-config,
+  wrapGAppsHook4,
+}:
+buildGoModule {
+  pname = "switchyard";
+  version = "dev";
+  src = ./.;
+  vendorHash = "sha256-1rEtf5QJA5xaeL0LRp13dwtPCH+DM5Wp4kJXBBqNtEg=";
+  subPackages = ["src"];
+
+  ldflags = [
+    "-s"
+    "-w"
+  ];
+
+  nativeBuildInputs = [
+    pkg-config
+    wrapGAppsHook4
+  ];
+
+  buildInputs = [
+    glib
+    gobject-introspection
+    gtk4
+    libadwaita
+    pkg-config
+  ];
+
+  postInstall = ''
+    install -Dm644 data/io.github.alyraffauf.Switchyard.desktop \
+      $out/share/applications/io.github.alyraffauf.Switchyard.desktop
+
+    install -Dm644 data/icons/hicolor/scalable/apps/io.github.alyraffauf.Switchyard.svg \
+      $out/share/icons/hicolor/scalable/apps/io.github.alyraffauf.Switchyard.svg
+  '';
+
+  meta = with lib; {
+    description = "A configurable default browser for Linux";
+    homepage = "https://github.com/alyraffauf/switchyard";
+    license = licenses.gpl3Plus;
+    platforms = platforms.linux;
+    mainProgram = "switchyard";
+  };
+}
